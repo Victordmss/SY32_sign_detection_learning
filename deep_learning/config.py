42a2ab5624bab1716a3fbc1bf5634fd2bb9b59a5
vdemessa <victor.demessance@etu.utc.fr>
2024-06-23 23:47:27 +0200

    [+] Add second deep architecture

--- deep_learning/config.py
+++ deep_learning/config.py
@@ -2,16 +2,16 @@ import torch
 import torchvision.transforms as transforms
 
 
-RESIZE_SIZE = (100, 100)
+RESIZE_SIZE = (64, 64)
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"  # Device 
 NB_EPOCHS = 30 # Number of epochs for training 
-BATCH_SIZE = 20  # Size of the training batch
+BATCH_SIZE = 15  # Size of the training batch
 TRAINING_IMAGE_FILE_PATH = "./data/train/images/"
 TRAINING_LABEL_FILE_PATH = "./data/train/labels/"
 VAL_IMAGE_FILE_PATH = "./data/val/images/"
 VAL_LABEL_FILE_PATH = "./data/val/labels/"
 
-TRANFORMATIONS = transforms.Compose([transforms.Resize((32,32)),
+TRANFORMATIONS = transforms.Compose([transforms.Resize((64,64)),
                                      transforms.ToTensor(),
                                      transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
                                                           std=[0.2023, 0.1994, 0.2010])
